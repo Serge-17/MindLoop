9cdeab7477e9a655c8ecb6cbd5bf64e5dc89f34f
Serge Eliseev <sergei.eliseev17@icloud.com>
2025-05-22 12:12:46 +0300

    Initial Commit

---
+++ MindLoop/MindLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindLoopApp.swift
+//  MindLoop
+//
+//  Created by Serge Eliseev on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MindLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindLoopTests/MindLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindLoopTests.swift
+//  MindLoopTests
+//
+//  Created by Serge Eliseev on 22.05.2025.
+//
+
+import Testing
+@testable import MindLoop
+
+struct MindLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
